9d7cd976a28b7f2321be59e57c3530938036de80
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-29 20:51:55 +0200

    Update dash.js

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -13,11 +13,23 @@ function getToken() {
 
 $(document).ready(function() {
     var token = getToken();
-    $.get({ url: "https://discordapp.com/api/users/@me", headers: { "Authorization": "Bearer " + token } }, function(data) {
+    $.get({
+        url: "https://discordapp.com/api/users/@me",
+        headers: {
+            "Authorization": "Bearer " + token,
+            crossDomain: true
+        }
+    }, function(data) {
         $(".u-a").attr('src', "https://cdn.discordapp.com/avatars/" + data.id + "/1faa3eed98189f795fda0674e9b96c29.png")
         $("#u-n").text("Hello " + data.username + "!");
     });
-    $.get({ url: "https://discordapp.com/api/users/@me/guilds", headers: { "Authorization": "Bearer " + token } }, function(data) {
+    $.get({
+        url: "https://discordapp.com/api/users/@me/guilds",
+        headers: {
+            "Authorization": "Bearer " + token,
+            crossDomain: true
+        }
+    }, function(data) {
         console.log(data);
         data.forEach(element => {
             if (element.owner)
