0485233488dfaf72acc8dbe9fda3ca143bfad491
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-29 15:39:54 +0200

    a

--- api/login.php
+++ api/login.php
@@ -1,78 +1,47 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('max_execution_time', 300);
-
-error_reporting(E_ALL);
-
 define('OAUTH2_CLIENT_ID', '747489983601836042');
 define('OAUTH2_CLIENT_SECRET', $_ENV["CS"]);
-
-$authorizeURL = 'https://discord.com/api/oauth2/authorize';
-$tokenURL = 'https://discord.com/api/oauth2/token';
-$apiURLBase = 'https://discord.com/api/users/@me';
-
-session_start();
-
-if(get('action') == 'login') {
-  $params = array(
-    'client_id' => OAUTH2_CLIENT_ID,
-    'redirect_uri' => 'https://dash.macedon.ga/api/login.php',
-    'response_type' => 'code',
-    'scope' => 'identify guilds'
-  );
-  header('Location: https://discordapp.com/api/oauth2/authorize' . '?' . http_build_query($params));
-  die();
-}
-
-if(get('code')) {
-  $token = apiRequest($tokenURL, array(
-    "grant_type" => "authorization_code",
-    'client_id' => OAUTH2_CLIENT_ID,
-    'client_secret' => OAUTH2_CLIENT_SECRET,
-    'redirect_uri' => 'https://dash.macedon.ga/api/index.php',
-    'code' => get('code')
-  ));
-  $logout_token = $token->access_token;
-  $_SESSION['access_token'] = $token->access_token;
-
-  header('Location: ' . $_SERVER['PHP_SELF']);
-}
-
-if(session('access_token')) {
-  header("location: https://dash.macedon.ga/api/index.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if (isset($_GET["error"])) {
+    echo json_encode(array("message" => "Authorization Error"));
+} elseif (isset($_GET["code"])) {
+    $redirect_uri = "your-request-URI-here";
+    $token_request = "https://discordapp.com/api/oauth2/token";
+    $token = curl_init();
+    curl_setopt_array($token, array(
+        CURLOPT_URL => $token_request,
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => array(
+            "grant_type" => "authorization_code",
+            "client_id" => "",
+            "client_secret" => "",
+            "redirect_uri" => $redirect_uri,
+            "code" => $_GET["code"]
+        )
+    ));
+    curl_setopt($token, CURLOPT_RETURNTRANSFER, true); 
+    $resp = json_decode(curl_exec($token));
+    curl_close($token);
+    if (isset($resp->access_token)) {
+        $access_token = $resp->access_token; 
+        $info_request = "https://discordapp.com/api/users/@me";
+        $info = curl_init();
+        curl_setopt_array($info, array(
+            CURLOPT_URL => $info_request,
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer {$access_token}"
+            ),
+            CURLOPT_RETURNTRANSFER => true
+        ));
+        $user = json_decode(curl_exec($info));
+        curl_close($info); 
+        echo "<h1>Hello, {$user->username}#{$user->discriminator}.</h1><br><h2>{$user->id}</h2><br><img src='https://discordapp.com/api/v6/users/{$user->id}/avatars/{$user->avatar}.jpg' /><br><br>Dashboard Token: {$access_token}"; 
+    } else {
+        echo json_encode(array("message" => "Authentication Error"));
+    }
 } else {
-  echo '<h3>Not logged in</h3>';
-  echo '<p><a href="?action=login">Log In</a></p>';
+    echo json_encode(array("message" => "No Code Provided"));
 }
-
-function apiRequest($url, $post=FALSE, $headers=array()) {
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-  $response = curl_exec($ch);
-
-  if($post)
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-
-  $headers[] = 'Accept: application/json';
-
-  if(session('access_token'))
-    $headers[] = 'Authorization: Bearer ' . session('access_token');
-
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-  $response = curl_exec($ch);
-  return json_decode($response);
-}
-
-function get($key, $default=NULL) {
-  return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
-}
-
-function session($key, $default=NULL) {
-  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
-}
-
 ?>
