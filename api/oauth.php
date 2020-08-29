eec7c15700bdd4a3363af0bc6e1950bed624791c
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-29 20:45:29 +0200

    Update oauth.php

--- api/oauth.php
+++ api/oauth.php
@@ -4,6 +4,6 @@
     } elseif (isset($_GET["code"])) {
         Header("Location: login.php?code={$_GET["code"]}");
     } else {
-      Header("Location: https://discord.com/api/oauth2/authorize?client_id=747489983601836042&redirect_uri=https%3A%2F%2Fdash.macedon.ga%2Fapi%2Flogin.php&response_type=code&scope=guilds%20identify");
+      Header("Location: https://discord.com/api/oauth2/authorize?client_id=747489983601836042&response_type=code&scope=guilds%20identify");
     }
 ?>
