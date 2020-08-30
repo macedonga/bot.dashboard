39d7c8d02237cc22df0cb260b689593836d92c35
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-30 17:25:01 +0200

    Update dash.js

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -7,9 +7,14 @@ $(document).ready(function() {
         $("#u-n").text("Hello " + ud.username + "!");
         $.get("https://dash.macedon.ga/api/discord.php?end=guilds", function(data) {
             const servers = JSON.parse(data);
+            var counter = 0;
             servers.forEach(server => {
-                if (server.owner)
-                    $('.content').append($("<button></button>").text(server.name).addClass("server"));
+                if (server.owner) {
+                    $('.content').append($("<button></button>").text(server.name).addClass("server").attr('id', server.id));
+                    counter++;
+                }
+                if (counter === 0)
+                    $('.content').append($("<h1></h1>").text("You aren't owner of any server.\nCreate a server and then add the bot.").addClass("animated"));
             });
         });
     });
