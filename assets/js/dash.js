a5b89776371766a7e294b7bb7d9f0595ecab9c83
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-30 18:57:09 +0200

    Update dash.js

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -10,13 +10,35 @@ $(document).ready(function() {
             var counter = 0;
             servers.forEach(server => {
                 if (server.owner) {
-                    $('.content').append($("<button></button>").text(server.name).addClass("server").attr('id', server.id));
-                    counter++;
+                    var button = $("<button></button>").text(server.name).addClass("server").attr('id', server.id);
+                    button.on("click", function() {
+                        var server = { id: server.id };
+                        $.ajax({
+                            url: "https://api.macedon.ga/mdbu/server/check",
+                            type: "POST",
+                            data: server,
+                            async: false,
+                            success: function(response, textStatus, jqXHR) {
+                                if (!response.connected)
+                                    alert("The bot isn't in the server you selected.\nInvite it first and then you can select this server.")
+                                else
+                                    location.href = "https://dash.macedon.ga/dash/manage?sid=" + server.id;
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                console.log(jqXHR);
+                                console.log(textStatus);
+                                console.log(errorThrown);
+                            }
+                        });
+                    });
+                    $('.content').append();
+                    counter = counter++;
                 }
-                if (counter === 0)
-                    $('.content').append($("<h1></h1>").text("You aren't owner of any server.\nCreate a server and then add the bot.").addClass("animated"));
             });
         });
+        if (counter === 0)
+            $('.content').append($("<h1></h1>").text("You aren't owner of any server.\nCreate a server and then add the bot.").addClass("animated"));
+
     });
 
 });
