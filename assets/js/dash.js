f52d76644137d38b86e1efe0db79b7c082b903b7
Marco Ceccon <m.ceccon05@gmail.com>
2020-08-30 14:59:10 +0200

    Fixed loading

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -1,16 +1,3 @@
-function getToken() {
-    var dbg = getUrlParameter("dbg");
-    if (dbg)
-        return localStorage.getItem("token");
-    $.get("https://dash.macedon.ga/api/get_token.php", function(data) {
-        if (data === "Not logged in")
-            location.href = "https://dash.macedon.ga/api/oauth.php";
-        else {
-            return data;
-        }
-    });
-}
-
 $(document).ready(function() {
     $.get("https://dash.macedon.ga/api/discord.php", function(data) {
         if (data === "Not logged in")
@@ -18,14 +5,26 @@ $(document).ready(function() {
         const ud = JSON.parse(data);
         $(".u-a").attr('src', "https://cdn.discordapp.com/avatars/" + ud.id + "/" + ud.avatar + ".png")
         $("#u-n").text("Hello " + ud.username + "!");
-    });
-    $.get("https://dash.macedon.ga/api/discord.php?end=guilds", function(data) {
-        if (data === "Not logged in")
-            return location.href = "https://dash.macedon.ga/api/oauth.php";
-        const servers = JSON.parse(data);
-        servers.forEach(server => {
-            if (server.owner)
-                $('.content').append($("<button></button>").text(server.name).addClass("server"));
+        $.get("https://dash.macedon.ga/api/discord.php?end=guilds", function(data) {
+            const servers = JSON.parse(data);
+            servers.forEach(server => {
+                if (server.owner)
+                    $('.content').append($("<button></button>").text(server.name).addClass("server"));
+            });
+            showGUI();
         });
     });
+
 });
+
+function showGUI() {
+    $(".loader").fadeIn(500, function() {
+            $(".loader").fadeOut(500, function() {
+                setTimeout(500, function() {
+                    $(".content").fadeIn(500);
+                    $(".header").fadeIn(500);
+                });
+            });
+        }
+    });
+}
